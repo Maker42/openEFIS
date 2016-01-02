9f177a694b28501a01edae5da5e0002b14969e9b
Garrett Herschleb <9.planetary.drive@gmail.com>
2016-01-02 09:12:43 -0300

    Missed checkin from last.

---
+++ ThrottleControl.py
@@ -0,0 +1,40 @@
+# Copyright (C) 2015  Garrett Herschleb
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>
+
+class ThrottleControl:
+    def __init__(self, throttle_table, nthrottles):
+        self.controller = None
+        self.last_val = 0.0
+        self._nthrottles = nthrottles
+        self._throttle_table = throttle_table
+
+    def SetServoController(self, c):
+        self.controller = c
+        self.controller.SetThrottleTable(self._throttle_table)
+
+    def Set(self, val):
+        self.last_val = val
+        self.controller.SetThrottles (val, self._nthrottles)
+
+    def GetCurrent(self):
+        return self.last_val
+
+    def GetLimits(self):
+        mn = 9999999
+        mx = -9999999
+        for k,v in self._throttle_table:
+            mn = mn if mn < k else k
+            mx = mx if mx > k else k
+        return (mn,mx)
